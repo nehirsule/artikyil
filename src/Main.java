f8ce525b76f93f6e2e69e2fca15f43d4f1d1404b
Şule Nehir ARAÇ <147002677+nehirsule@users.noreply.github.com>
2024-03-06 11:50:13 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class Main {
-}
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int year;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Sorgulamak istediğiniz yılı girin :");
+        year = input.nextInt();
+
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0) {
+                    System.out.println(year + "  yılı artık yıldır");
+                } else {
+                    System.out.println(year + "  yılı artık yıl değildir.");
+                }
+            } else {
+                System.out.println(year + "  yılı artık yılldır.");
+            }
+
+        } else {
+            System.out.println(year + "  artık yıl değildir");
+        }
+    }
